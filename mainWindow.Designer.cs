632856c4dd4e85853716991a6ec55ddf9930e6e7
Julius Rupsys <julius.rupsys98@gmail.com>
2018-09-20 18:10:27 +0300

    Renamed project, added new empty form

--- mainWindow.Designer.cs
+++ mainWindow.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Librius
+﻿namespace MyLibrarian
 {
-    partial class signInWindow
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,94 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.registerButton = new System.Windows.Forms.Button();
-            this.userIdTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.passwordField = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // registerButton
-            // 
-            this.registerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.registerButton.Location = new System.Drawing.Point(138, 158);
-            this.registerButton.Name = "registerButton";
-            this.registerButton.Size = new System.Drawing.Size(90, 30);
-            this.registerButton.TabIndex = 0;
-            this.registerButton.Text = "Register";
-            this.registerButton.UseVisualStyleBackColor = true;
-            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
-            // 
-            // userIdTextBox
-            // 
-            this.userIdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userIdTextBox.Location = new System.Drawing.Point(26, 68);
-            this.userIdTextBox.MaxLength = 7;
-            this.userIdTextBox.Name = "userIdTextBox";
-            this.userIdTextBox.Size = new System.Drawing.Size(202, 26);
-            this.userIdTextBox.TabIndex = 1;
-            this.userIdTextBox.Enter += new System.EventHandler(this.userIdGetFocus);
-            this.userIdTextBox.Leave += new System.EventHandler(this.userIdLoseFocus);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(78, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 31);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Sign In";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // passwordField
-            // 
-            this.passwordField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordField.Location = new System.Drawing.Point(26, 112);
-            this.passwordField.MaxLength = 14;
-            this.passwordField.Name = "passwordField";
-            this.passwordField.PasswordChar = '*';
-            this.passwordField.Size = new System.Drawing.Size(202, 26);
-            this.passwordField.TabIndex = 3;
-            this.passwordField.Enter += new System.EventHandler(this.passwordGetFocus);
-            this.passwordField.Leave += new System.EventHandler(this.passwordLoseFocus);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(26, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 30);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Log In";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // signInWindow
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 200);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.passwordField);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.userIdTextBox);
-            this.Controls.Add(this.registerButton);
-            this.Name = "signInWindow";
-            this.RightToLeftLayout = true;
-            this.Text = "Virtual Library";
-            this.Load += new System.EventHandler(this.signInWindow_Load);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormClosed);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button registerButton;
-        private System.Windows.Forms.TextBox userIdTextBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox passwordField;
-        private System.Windows.Forms.Button button1;
     }
 }
-
