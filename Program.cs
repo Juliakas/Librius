896fc09a67f9d4ab1e2952983039b135097aed0e
Karolis Rutkauskas <karolisrutkauskas1@gmail.com>
2018-09-23 20:11:42 +0300

    .

--- Program.cs
+++ Program.cs
@@ -14,47 +14,13 @@ namespace MyLibrarian
         /// </summary>
 
         //"Data Source =.\\SQLEXPRESS; Database = LibraryDatabase; Integrated Security = True"
+        //Properties.Settings.Default.LibraryDatabaseConnectionString;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            string connS = Properties.Settings.Default.LibraryDatabaseConnectionString;
-            SqlConnection conn = new SqlConnection(connS);
-            try
-            {
-                conn.Open();
-                Console.WriteLine("Open");
-                //SqlCommand comm = new SqlCommand("insert into skaitytojas (ID, Vardas, Pavarde) values (@id, @name, @surname)", conn);
-
-                /*
-                int i = 0;
-                SqlCommand comm = new SqlCommand("delete from skaitytojas", conn);
-                comm.ExecuteNonQuery();
-                while(i < 3)
-                {
-                    comm = new SqlCommand("insert into skaitytojas (ID, Vardas, Pavarde) values (@id, @name, @surname)", conn);
-                    comm.Parameters.AddWithValue("@id", Console.ReadLine());
-                    comm.Parameters.AddWithValue("@name", Console.ReadLine());
-                    comm.Parameters.AddWithValue("@surname", Console.ReadLine());
-                    comm.ExecuteNonQuery();
-                    i++;
-                }
-                */
-
-                SqlCommand comm = new SqlCommand("select * from skaitytojas", conn);
-                SqlDataReader reader = comm.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetInt32(0));
-                    Console.WriteLine(reader.GetString(1));
-                    Console.WriteLine(reader.GetString(2));
-                }
-                conn.Close();
-            }catch(Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
             Application.Run(new MainWindow());
         }
     }
