e29e707373fcf73c7ce8109a30f21ff4718718f7
DESKTOP-9NGHTCS\Gabija <gabija.savukynaite@gmail.com>
2018-11-14 16:43:24 +0200

    Created frontend

---
+++ MyLibrarianFrontend/MyLibrarianFrontend/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyLibrarianFrontend
+{
+    public class Book
+    {
+        public string ISBN { get; private set; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public DateTime Date { get; private set; }
+     
+
+        public Book(string isbn, string title, string author, DateTime date)
+        {
+            this.ISBN = isbn;
+            this.Title = title;
+            this.Author = author;
+            this.Date = date;
+        }
+    }
+
+}
