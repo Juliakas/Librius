6fd90c4431d28016e80ae76061cf1b1c7f98f5bf
Julius Rupsys <julius.rupsys98@gmail.com>
2018-11-20 22:46:25 +0200

    Revert "Added everything"
    This reverts commit a5212e81e440e2434bfe1384056643fcce2e7d68.

--- LibraryService/App_Start/FilterConfig.cs
+++ LibraryService/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace LibraryService
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute());
         }
     }
 }
